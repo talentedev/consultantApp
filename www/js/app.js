441ef498527f4a43bc9d45b48402f50a8523dc9b
talentedev <33281524+talentedev@users.noreply.github.com>
2017-11-27 09:02:17 -0800

    init

---
+++ www/js/app.js
@@ -0,0 +1,272 @@
+﻿// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+angular.module('ctApp', ['ionic', 'ctApp.controllers', 'ctApp.services', 'ng-fusioncharts'])
+
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    if (cordova.platformId === "ios" && window.cordova && window.cordova.plugins.Keyboard) {
+      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+      // for form inputs)
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+
+      // Don't remove this line unless you know what you are doing. It stops the viewport
+      // from snapping when text inputs are focused. Ionic handles this internally for
+      // a much nicer keyboard experience.
+      cordova.plugins.Keyboard.disableScroll(true);
+    }
+    if(window.StatusBar) {
+      StatusBar.styleDefault();
+    }
+  });
+})
+.config(function ($stateProvider, $urlRouterProvider) {
+
+    // Ionic uses AngularUI Router which uses the concept of states
+    // Learn more here: https://github.com/angular-ui/ui-router
+    // Set up the various states which the app can be in.
+    // Each state's controller can be found in controllers.js
+$stateProvider
+//login page
+.state('login', {
+    url: '/login',
+    templateUrl: 'templates/login.html',
+    controller: 'loginCtrl'
+})
+// setup an abstract state for the tabs directive
+.state('tab', {
+    url: '/tab',
+    abstract: true,
+    templateUrl: 'templates/tabs.html'
+})
+/*消息*/
+.state('tab.news', {
+    url: '/news',
+    views: {
+        'tab-news': {
+            templateUrl: 'templates/tab-news.html',
+            controller: 'newsCtrl'
+        }
+    }
+})
+/*自我管理*/
+.state('tab.manage', {
+    url: '/manage',
+    views: {
+        'tab-manage': {
+            templateUrl: 'templates/tab-manage.html',
+            controller: 'manageCtrl'
+        }
+    }
+})
+/*日历*/
+.state('tab.date', {
+    url: '/date',
+    views: {
+        'tab-date': {
+            templateUrl: 'templates/tab-date.html',
+            controller: 'dateCtrl'
+        }
+    }
+})
+/*门店*/
+.state('tab.store', {
+    url: '/store',
+    views: {
+        'tab-store': {
+            templateUrl: 'templates/tab-store.html',
+            controller: 'storeCtrl'
+        }
+    }
+})
+/*我的*/
+.state('tab.self', {
+    url: '/self',
+    views: {
+        'tab-self': {
+            templateUrl: 'templates/tab-self.html',
+            controller: 'selfCtrl'
+        }
+    }
+})
+/*公告*/
+.state('news-announcement', {
+    url: '/news-announcement',
+    templateUrl: 'templates/news-announcement.html',
+    controller: 'news-announcementCtrl'
+})
+/*企业网盘*/
+.state('news-dynamic', {
+    url: '/news-dynamic',
+    templateUrl: 'templates/news-dynamic.html',
+    controller: 'news-dynamicCtrl'
+})
+/*月度沟通文件*/
+.state('news-dynamic-detail', {
+    url: '/news-dynamic-detail',
+    templateUrl: 'templates/news-dynamic-detail.html',
+    controller: 'news-dynamic-detailCtrl'
+})
+/*月度沟通文件*/
+.state('news-dynamic-detail-pptx', {
+    url: '/news-dynamic-detail-pptx',
+    templateUrl: 'templates/news-dynamic-detail-pptx.html',
+    controller: 'news-dynamic-detail-pptxCtrl'
+})
+/*跟踪提醒*/
+.state('news-tracking', {
+    url: '/news-tracking',
+    templateUrl: 'templates/news-tracking.html',
+    controller: 'news-trackingCtrl'
+})
+/*客服*/
+.state('news-customer', {
+    url: '/news-customer',
+    templateUrl: 'templates/news-customer.html',
+    controller: 'news-customerCtrl'
+})
+/*添加/修改门店*/
+.state('store-detail', {
+    url: '/store-detail',
+    templateUrl: 'templates/store-detail.html',
+    controller: 'store-detailCtrl'
+})
+/*查看店面人员信息*/
+.state('store-manager', {
+    url: '/store-manager',
+    templateUrl: 'templates/store-manager.html',
+    controller: 'store-managerCtrl'
+})
+/*个人信息*/
+.state('self-personal', {
+    url: '/self-personal',
+    templateUrl: 'templates/self-personal.html',
+    controller: 'self-personalCtrl'
+})
+/*执行巡店类行程*/
+.state('perform', {
+    url: '/perform',
+    templateUrl: 'templates/perform.html',
+    controller: 'performCtrl'
+})
+/*外勤签到*/
+.state('perform-field', {
+    url: '/perform-field',
+    templateUrl: 'templates/perform-field.html',
+    controller: 'perform-fieldCtrl'
+})
+/*SWOT分析*/
+.state('perform-analysis', {
+    url: '/perform-analysis',
+    templateUrl: 'templates/perform-analysis.html',
+    controller: 'perform-analysisCtrl'
+})
+/*店面年度工作计划*/
+.state('perform-storefront', {
+    url: '/perform-storefront',
+    templateUrl: 'templates/perform-storefront.html',
+    controller: 'perform-storefrontCtrl'
+})
+/*添加行动计划及草案制定*/
+.state('perform-develop', {
+    url: '/perform-develop',
+    templateUrl: 'templates/perform-develop.html',
+    controller: 'perform-developCtrl'
+})
+/*驰加审计结果*/
+.state('perform-audit', {
+    url: '/perform-audit',
+    templateUrl: 'templates/perform-audit.html',
+    controller: 'perform-auditCtrl'
+})
+/*拍照上传*/
+.state('perform-upload', {
+    url: '/perform-upload',
+    templateUrl: 'templates/perform-upload.html',
+    controller: 'perform-uploadCtrl'
+})
+/*Camera*/
+.state('perform-camera', {
+    url: '/perform-camera',
+    templateUrl: 'templates/perform-camera.html',
+    controller: 'perform-cameraCtrl'
+})
+/*添加行程*/
+.state('perform-trip', {
+    url: '/perform-trip',
+    templateUrl: 'templates/perform-trip.html',
+    controller: 'perform-tripCtrl'
+})
+/*库存上报*/
+.state('perform-inventory', {
+    url: '/perform-inventory',
+    templateUrl: 'templates/perform-inventory.html',
+    controller: 'perform-inventoryCtrl'
+})
+/*销量上报*/
+.state('perform-sales', {
+    url: '/perform-sales',
+    templateUrl: 'templates/perform-sales.html',
+    controller: 'perform-salesCtrl'
+})
+/*订单上报*/
+.state('perform-orders', {
+    url: '/perform-orders',
+    templateUrl: 'templates/perform-orders.html',
+    controller: 'perform-ordersCtrl'
+})
+/*竞品上报*/
+.state('perform-competing', {
+    url: '/perform-competing',
+    templateUrl: 'templates/perform-competing.html',
+    controller: 'perform-competingCtrl'
+})
+/*添加/查看/修改行动计划*/
+.state('perform-action', {
+    url: '/perform-action',
+    templateUrl: 'templates/perform-action.html',
+    controller: 'perform-actionCtrl'
+})
+/*添加沟通*/
+.state('perform-communication', {
+    url: '/perform-communication',
+    templateUrl: 'templates/perform-communication.html',
+    controller: 'perform-communicationCtrl'
+})
+/*路途*/
+.state('road', {
+    url: '/road',
+    templateUrl: 'templates/road.html',
+    controller: 'roadCtrl'
+})
+/*添加行程*/
+.state('trip', {
+    url: '/trip',
+    templateUrl: 'templates/trip.html',
+    controller: 'tripCtrl'
+})
+/*选择行程类型*/
+.state('trip-type', {
+    url: '/trip-type',
+    templateUrl: 'templates/trip-type.html',
+    controller: 'trip-typeCtrl'
+})
+/*选择成员*/
+.state('trip-select', {
+    url: '/trip-select',
+    templateUrl: 'templates/trip-select.html',
+    controller: 'trip-selectCtrl'
+})
+/*选择门店*/
+.state('trip-store', {
+    url: '/trip-store',
+    templateUrl: 'templates/trip-store.html',
+    controller: 'trip-storeCtrl'
+})
+
+    // if none of the above states are matched, use this as the fallback
+$urlRouterProvider.otherwise('/login');
+
+});
---
+++ www/js/controllers.js
@@ -0,0 +1,502 @@
+﻿angular.module('ctApp.controllers', [])
+
+ /*the controller that manage about user login*/
+.controller('loginCtrl', function ($scope, $state) {
+
+    $scope.login = function (user) {
+        console.log('Log-In', user);
+        $state.go('tab.date');
+    };
+
+})
+/*the controller that manage user's news*/
+.controller('newsCtrl', function ($scope, $state) {
+    $scope.go_announcement = function () {
+        $state.go('news-announcement');
+    }
+    $scope.go_dynamic = function () {
+        $state.go('news-dynamic');
+    }
+    $scope.go_tabdate = function () {
+        $state.go('tab.date');
+    }
+    $scope.go_tracking = function () {
+        $state.go('news-tracking')
+    }
+    $scope.go_customer = function () {
+        $state.go('news-customer')
+    }
+    $scope.go_perform = function () {
+        $state.go('perform')
+    }
+})
+/*user management chart controller*/
+.controller('manageCtrl', function ($scope, $state) {
+    $scope.myDataSource1 = {
+        chart: {
+            "borderColor": "#ffffff",
+            "borderThickness": "0",
+            "borderAlpha": "0",
+            "bgColor": "#ffffff",
+            "usePlotGradientColor": "0",
+            "showPlotBorder": "0",
+            "labelFontSize": "11",
+            "labelFontColor": "#000000",
+            "labelFontBold": "1",
+            "valueFontColor": "#48b52d",
+            "showYAxisValues": "0",
+            "divLineColor": "#ffffff",
+            "showAlternateHGridColor": "0",
+            "showCanvasBorder": "0",
+            "chartLeftMargin": "0",
+            "chartRightMargin": "0",
+            "plotSpacePercent": "40",
+
+        },
+        data: [{
+            label: "一月",
+            value: "9",
+            color: "#48b52d"
+        }, {
+            label: "二月",
+            value: "12",
+            color: "#48b52d"
+        }, {
+            label: "三月",
+            value: "12",
+            color: "#48b52d"
+        }, {
+            label: "四月",
+            value: "12",
+            color: "#48b52d"
+        }, {
+            label: "五月",
+            value: "12",
+            color: "#48b52d"
+        }, {
+            label: "六月",
+            value: "12",
+            color: "#48b52d"
+        }, {
+            label: "七月",
+            value: "9",
+            color: "#48b52d"
+        }, {
+            label: "八月",
+            value: "20",
+            color: "#48b52d"
+        }, {
+            label: "九月",
+            value: "",
+            color: "#48b52d"
+        }, {
+            label: "十月",
+            value: "",
+            color: "#48b52d"
+        }, {
+            label: "十一",
+            value: "",
+            color: "#48b52d"
+        }, {
+            label: "十二",
+            value: "",
+            color: "#48b52d"
+        }]
+    };
+    $scope.myDataSource2 = {
+        chart: {
+            "borderColor": "#ffffff",
+            "borderThickness": "0",
+            "borderAlpha": "0",
+            "bgColor": "#ffffff",
+            "usePlotGradientColor": "0",
+            "showPlotBorder": "0",
+            "labelFontSize": "11",
+            "labelFontColor": "#000000",
+            "labelFontBold": "1",
+            "valueFontColor": "#48b52d",
+            "showYAxisValues": "0",
+            "divLineColor": "#ffffff",
+            "showAlternateHGridColor": "0",
+            "showCanvasBorder": "0",
+            "chartLeftMargin": "0",
+            "chartRightMargin": "0",
+            "plotSpacePercent": "40",
+            "placevaluesInside": "0",
+            "yAxisMaxValue": "400",
+        },
+        data: [{
+            label: "一月",
+            value: "190",
+            color: "#48b52d"
+        }, {
+            label: "二月",
+            value: "92",
+            color: "#48b52d"
+        }, {
+            label: "三月",
+            value: "174",
+            color: "#48b52d"
+        }, {
+            label: "四月",
+            value: "148",
+            color: "#48b52d"
+        }, {
+            label: "五月",
+            value: "179",
+            color: "#48b52d"
+        }, {
+            label: "六月",
+            value: "179",
+            color: "#48b52d",
+        }, {
+            label: "七月",
+            value: "150",
+            color: "#48b52d"
+        }, {
+            label: "八月",
+            value: "160",
+            color: "#48b52d"
+        }, {
+            label: "九月",
+            value: "",
+            color: "#48b52d"
+        }, {
+            label: "十月",
+            value: "",
+            color: "#48b52d"
+        }, {
+            label: "十一",
+            value: "",
+            color: "#48b52d"
+        }, {
+            label: "十二",
+            value: "",
+            color: "#48b52d"
+        }]
+    };
+})
+/*the controller that manage date*/
+.controller('dateCtrl', function ($scope, $state, $ionicHistory, Calendar) {
+    $scope.$on('$ionicView.enter', function(event){
+        Calendar.initial();
+    })
+    $scope.year = Calendar.getyear();
+    $scope.month = Calendar.getmonth();
+    $scope.pre_calendar = function () {
+        if ($scope.month == 1) {
+            $scope.month = 12;
+            $scope.year = $scope.year - 1;
+        } else {
+            $scope.month = $scope.month -1;
+        }
+        Calendar.changeCal($scope.year,$scope.month)
+    }
+    $scope.next_calendar = function () {
+        if ($scope.month == 12) {
+            $scope.month = 1;
+            $scope.year = $scope.year + 1;
+        } else {
+            $scope.month = $scope.month + 1;
+        }
+        Calendar.changeCal($scope.year, $scope.month)
+    }
+    $scope.go_back = function () {
+        $ionicHistory.goBack();
+    }
+    $scope.go_perform = function () {
+        $state.go('perform');
+    }
+    $scope.go_road = function () {
+        $state.go('road');
+    }
+    $scope.go_trip = function () {
+        $state.go('trip');
+    }
+})
+/*the controller that manage store*/
+.controller('storeCtrl', function ($scope, $state, $ionicHistory) {
+    $scope.go_back = function () {
+        $ionicHistory.goBack();
+    }
+    $scope.go_detail = function () {
+        $state.go('store-detail');
+    }
+    $scope.go_add = function () {
+        $state.go('store-detail');
+    }
+})
+/*the controller that manage self data*/
+.controller('selfCtrl', function ($scope, $state, $ionicHistory) {
+    $scope.go_back = function () {
+        $ionicHistory.goBack();
+    }
+    $scope.go_information = function () {
+        $state.go('self-personal')
+    }
+})
+/*the controller that manage announcement data*/
+.controller('news-announcementCtrl', function ($scope, $state, $ionicHistory) {
+    $scope.go_back = function () {
+        $ionicHistory.goBack();
+    }
+})
+/*the controller that manage dynamic file data*/
+.controller('news-dynamicCtrl', function ($scope, $state, $ionicHistory) {
+    $scope.go_back = function () {
+        $ionicHistory.goBack();
+    }
+    $scope.go_detail = function () {
+        $state.go('news-dynamic-detail')
+    }
+})
+/*the controller that manage dynamic file data*/
+.controller('news-dynamic-detailCtrl', function ($scope, $state, $ionicHistory) {
+    $scope.go_back = function () {
+        $ionicHistory.goBack();
+    }
+    $scope.go_detail = function () {
+        $state.go('news-dynamic-detail-pptx')
+    }
+})
+/*the controller that manage dynamic pptx file data*/
+.controller('news-dynamic-detail-pptxCtrl', function ($scope, $state, $ionicHistory) {
+    $scope.go_back = function () {
+        $ionicHistory.goBack();
+    }
+})
+/*the controller that manage tracking reminder data*/
+.controller('news-trackingCtrl', function ($scope, $state, $ionicHistory) {
+    $scope.go_back = function () {
+        $ionicHistory.goBack();
+    }
+})
+/*the controller that manage customer service data*/
+.controller('news-customerCtrl', function ($scope, $state, $ionicHistory) {
+    $scope.go_back = function () {
+        $ionicHistory.goBack();
+    }
+})
+
+/*the controller that add/edit store data*/
+.controller('store-detailCtrl', function ($scope, $state, $ionicHistory) {
+    $scope.go_back = function () {
+        $ionicHistory.goBack();
+    }
+    $scope.go_save = function () {
+        $state.go('tab.store');
+    }
+    $scope.go_manager = function () {
+        $state.go('store-manager');
+    }
+})
+/*the controller that manage perform store inspection class trip infomation data*/
+.controller('store-managerCtrl', function ($scope, $state, $ionicHistory) {
+    $scope.go_back = function () {
+        $ionicHistory.goBack();
+    }
+})
+/*the controller that manage personal infomation data*/
+.controller('self-personalCtrl', function ($scope, $state, $ionicHistory) {
+    $scope.go_back = function () {
+        $ionicHistory.goBack();
+    }
+    $scope.go_save = function () {
+        $state.go('tab.self');
+    }
+})
+
+/*the controller that manage perform store inspection class trip infomation data*/
+.controller('performCtrl', function ($scope, $state, $ionicHistory) {
+    $scope.go_back = function () {
+        $ionicHistory.goBack();
+    }
+    $scope.go_field = function () {
+        $state.go('perform-field');
+    }
+    $scope.go_analysis = function () {
+        $state.go('perform-analysis');
+    }
+    $scope.go_storefront = function () {
+        $state.go('perform-storefront');
+    }
+    $scope.go_store = function () {
+        $state.go('store-detail');
+    }
+    $scope.go_develop = function () {
+        $state.go('perform-develop');
+    }
+    $scope.go_audit = function () {
+        $state.go('perform-audit');
+    }
+    $scope.go_upload = function () {
+        $state.go('perform-upload');
+    }
+    $scope.go_trip = function () {
+        $state.go('perform-trip');
+    }
+    $scope.go_inventory = function () {
+        $state.go('perform-inventory');
+    }
+    $scope.go_sales = function () {
+        $state.go('perform-sales');
+    }
+    $scope.go_orders = function () {
+        $state.go('perform-orders');
+    }
+    $scope.go_competing = function () {
+        $state.go('perform-competing');
+    }
+    $scope.go_action = function () {
+        $state.go('perform-action');
+    }
+    $scope.go_communication = function () {
+        $state.go('perform-communication');
+    }
+    $scope.go_training = function () {
+        $state.go('perform-training');
+    }
+    $scope.go_customer = function () {
+        $state.go('news-customer');
+    }
+})
+/*the controller that manage filed sign data*/
+.controller('perform-fieldCtrl', function ($scope, $state, $ionicHistory) {
+    $scope.go_back = function () {
+        $ionicHistory.goBack();
+    }
+    $scope.go_save = function () {
+        $state.go('tab.self');
+    }
+})
+/*the controller that manage analysis data*/
+.controller('perform-analysisCtrl', function ($scope, $state, $ionicHistory) {
+    $scope.go_back = function () {
+        $ionicHistory.goBack();
+    }
+
+})
+/*the controller that manage Storefront annual work plan data*/
+.controller('perform-storefrontCtrl', function ($scope, $state, $ionicHistory) {
+    $scope.go_back = function () {
+        $ionicHistory.goBack();
+    }
+
+})
+/*Add to develop action plans and draft*/
+.controller('perform-developCtrl', function ($scope, $state, $ionicHistory) {
+    $scope.go_back = function () {
+        $ionicHistory.goBack();
+    }
+
+})
+/*Chi added audit findings*/
+.controller('perform-auditCtrl', function ($scope, $state, $ionicHistory) {
+    $scope.go_back = function () {
+        $ionicHistory.goBack();
+    }
+
+})
+/*Upload pictures*/
+.controller('perform-uploadCtrl', function ($scope, $state, $ionicHistory) {
+    $scope.go_back = function () {
+        $ionicHistory.goBack();
+    }
+    $scope.go_camera = function () {
+        $state.go('perform-camera');
+    }
+
+})
+/*Camera*/
+.controller('perform-cameraCtrl', function ($scope, $state, $ionicHistory) {
+    $scope.go_back = function () {
+        $ionicHistory.goBack();
+    }
+})
+/*Add trip*/
+.controller('perform-tripCtrl', function ($scope, $state, $ionicHistory) {
+    $scope.go_back = function () {
+        $ionicHistory.goBack();
+    }
+
+})
+/*Inventory reporting*/
+.controller('perform-inventoryCtrl', function ($scope, $state, $ionicHistory) {
+    $scope.go_back = function () {
+        $ionicHistory.goBack();
+    }
+
+})
+/*Sales reporting*/
+.controller('perform-salesCtrl', function ($scope, $state, $ionicHistory) {
+    $scope.go_back = function () {
+        $ionicHistory.goBack();
+    }
+
+})
+/*Orders submitted*/
+.controller('perform-ordersCtrl', function ($scope, $state, $ionicHistory) {
+    $scope.go_back = function () {
+        $ionicHistory.goBack();
+    }
+
+})
+/*Competing products reported*/
+.controller('perform-competingCtrl', function ($scope, $state, $ionicHistory) {
+    $scope.go_back = function () {
+        $ionicHistory.goBack();
+    }
+
+})
+/*Add / View / Edit Action Plan*/
+.controller('perform-actionCtrl', function ($scope, $state, $ionicHistory) {
+    $scope.go_back = function () {
+        $ionicHistory.goBack();
+    }
+
+})
+/*Adding communication*/
+.controller('perform-communicationCtrl', function ($scope, $state, $ionicHistory) {
+    $scope.go_back = function () {
+        $ionicHistory.goBack();
+    }
+})
+/*Road*/
+.controller('roadCtrl', function ($scope, $state, $ionicHistory) {
+    $scope.go_back = function () {
+        $ionicHistory.goBack();
+    }
+})
+/*Trip*/
+.controller('tripCtrl', function ($scope, $state, $ionicHistory) {
+    $scope.go_back = function () {
+        $ionicHistory.goBack();
+    }
+    $scope.go_type = function () {
+        $state.go('trip-type');
+    }
+    $scope.go_select = function () {
+        $state.go('trip-select');
+    }
+    $scope.go_store = function () {
+        $state.go('trip-store');
+    }
+})
+/*Trip-type*/
+.controller('trip-typeCtrl', function ($scope, $state, $ionicHistory) {
+    $scope.go_back = function () {
+        $ionicHistory.goBack();
+    }
+})
+/*Trip-select*/
+.controller('trip-selectCtrl', function ($scope, $state, $ionicHistory) {
+    $scope.go_back = function () {
+        $ionicHistory.goBack();
+    }
+})
+/*Trip-store*/
+.controller('trip-storeCtrl', function ($scope, $state, $ionicHistory) {
+    $scope.go_back = function () {
+        $ionicHistory.goBack();
+    }
+})
